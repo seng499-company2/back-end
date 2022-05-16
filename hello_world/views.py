b3fd2988b21f10ee10bd0c9a76fc65c505ba8b7f
Sam Kosman <41757019+skosman@users.noreply.github.com>
2022-05-16 05:49:16 -0700

    Revert "Create hello world django app"

--- hello_world/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-from .serializers import WorldSerializer
-from .models import World
- 
-
-class ListWorldView(generics.ListAPIView):
-    # define queryset
-    queryset = World.objects.all()
-    serializer_class = WorldSerializer
