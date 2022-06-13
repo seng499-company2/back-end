3350de64902a72608da29a3d649ecfb3bd6a494d
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-06-13 11:47:55 -0700

    user views checks for authenticated user making request

--- users/test_views.py
+++ users/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase  # **tests that interact with database require 
 from django.contrib.auth.models import User
 from .serializers import AppUserSerializer
 from .models import AppUser
-from .views import ProfessorsList
+from .views import ProfessorsList, get_auth_errors
 from .views import Professor
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
@@ -53,13 +53,15 @@ class TestProfessorsList(TestCase):
         self.save_default_user()
         request_factory = APIRequestFactory()
         request = request_factory.post('/users/abcdef', data=self.default_serializer_data, format='json')
+        request.user = User.objects.create_user("admin", is_superuser=True)
         response = Professor().post(request, requested_pk='abcdef')
         self.assertIsNotNone(response)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
-    def test_prof_createion_POST(self):
+    def test_prof_creation_POST(self):
         request_factory = APIRequestFactory()
         request = request_factory.post('/users/', data=self.default_serializer_data, format='json')
+        request.user = User.objects.create_user("admin", is_superuser=True)
         response = ProfessorsList().post(request)
         self.assertIsNotNone(response)
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
@@ -68,6 +70,7 @@ class TestProfessorsList(TestCase):
         self.save_default_user()
         request_factory = APIRequestFactory()
         request = request_factory.get('/users/')
+        request.user = User.objects.create_user("admin", is_superuser=True)
         response: HttpResponse = ProfessorsList().get(request)
         self.assertIsNotNone(response)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
@@ -76,9 +79,49 @@ class TestProfessorsList(TestCase):
         self.assertContains(response, "\"user\": {\"username\": \"abcdef\"")
 
     def test_prof_DELETE(self):
-        self.save_default_user()
         request_factory = APIRequestFactory()
         request = request_factory.delete('/users/abcdef')
+        request.user = User.objects.create_user("admin", is_superuser=True)
+        self.save_default_user()
         response = Professor().delete(request, requested_pk='abcdef')
         self.assertIsNotNone(response)
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+    def test_prof_DELETE__not_found(self):
+        request_factory = APIRequestFactory()
+        request = request_factory.delete('/users/doesNotExist')
+        self.save_default_user()
+        request.user = User.objects.create_user("admin", is_superuser=True)
+        response = Professor().delete(request, requested_pk='doesNotExist')
+        self.assertIsNotNone(response)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    def test_get_auth_errors_not_superuser(self):
+        request_factory = APIRequestFactory()
+        request = request_factory.delete('/users/abcdef')
+        request.user = User.objects.create_user("not_superuser", is_superuser=False)
+        auth_error = get_auth_errors(request)
+        self.assertIsNotNone(auth_error)
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, auth_error.status_code)
+
+    def test_get_auth_errors_User(self):
+        request_factory = APIRequestFactory()
+        request = request_factory.delete('/users/abcdef')
+        request.user = User.objects.create_user("admin", is_superuser=True)
+        auth_error = get_auth_errors(request)
+        self.assertIsNone(auth_error)
+
+    def test_get_auth_errors_AppUser(self):
+        request_factory = APIRequestFactory()
+        request = request_factory.delete('/users/abcdef')
+        user = User.objects.create_user("admin", is_superuser=True)
+        request.user = AppUser.objects.create(user=user)
+        auth_error = get_auth_errors(request)
+        self.assertIsNone(auth_error)
+
+    def test_get_auth_errors_no_user(self):
+        request_factory = APIRequestFactory()
+        request = request_factory.delete('/users/abcdef')
+        auth_error = get_auth_errors(request)
+        self.assertIsNotNone(auth_error)
+        self.assertEqual(status.HTTP_500_INTERNAL_SERVER_ERROR, auth_error.status_code)
--- users/views.py
+++ users/views.py
@@ -1,14 +1,27 @@
 import json
 
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework import status
 
+import users
 from .models import AppUser
 from .serializers import AppUserSerializer
+
+
+def get_auth_errors(request: HttpRequest) -> HttpResponse | None:
+    # in a try block because if authenticating with token request.user doesn't even exist
+    try:
+        # At time of development, Not sure if request.user will be a django.contrib.auth.models
+        # or an AppUser. Both cases covered
+        caller = request.user.user if isinstance(request.user, AppUser) else request.user
+        if (not caller.is_authenticated) or (not caller.is_superuser):
+            return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
+    except AttributeError:
+        return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return None
 
 
 class ProfessorsList(APIView):
@@ -20,7 +33,10 @@ class ProfessorsList(APIView):
     def get(self, request):
         if request.method != "GET":
             return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        # TODO: Check for admin in request
+
+        authentication_error = get_auth_errors(request)
+        if authentication_error is not None:
+            return authentication_error
 
         # get all non-admin AppUsers **may have to also fetch User parent class + concatenate fields**
         profs_list = AppUser.objects.filter(user__is_superuser=False)
@@ -31,7 +47,11 @@ class ProfessorsList(APIView):
     def post(self, request: HttpRequest, format=None) -> HttpResponse:
         if request.method != "POST":
             return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        # TODO: Check for admin in request
+
+        authentication_error = get_auth_errors(request)
+        if authentication_error is not None:
+            return authentication_error
+
         request_data = JSONParser().parse(request)
         serializer = AppUserSerializer(data=request_data)
         if serializer.is_valid():
@@ -50,10 +70,15 @@ class Professor(APIView):
         if request.method != "POST":
             return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # TODO: Check for admin in request
+        authentication_error = get_auth_errors(request)
+        if authentication_error is not None:
+            return authentication_error
 
-        prof = AppUser.objects.get(user__username=requested_pk)
-        if prof is None or not isinstance(prof, AppUser):
+        try:
+            prof = AppUser.objects.get(user__username=requested_pk)
+            if prof is None or not isinstance(prof, AppUser):
+                return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        except users.models.AppUser.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
         request_data = JSONParser().parse(request)
         serializer = AppUserSerializer(prof, data=request_data)
@@ -67,10 +92,12 @@ class Professor(APIView):
         if request.method != "DELETE":
             return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # TODO: Check for admin in request
-
-        prof = AppUser.objects.get(user__username=requested_pk)
-        if prof is None or not isinstance(prof, AppUser):
+        authentication_error = get_auth_errors(request)
+        if authentication_error is not None:
+            return authentication_error
+        try:
+            prof = AppUser.objects.get(user__username=requested_pk)
+        except users.models.AppUser.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
         prof.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
