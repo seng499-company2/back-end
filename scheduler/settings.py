35d54b2c227393aaed1f1e6ee6bc538e9866a567
Samantha Kosman <skosman@uvic.ca>
2022-06-16 19:04:47 -0700

    Have default configuration for drf

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -134,8 +134,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
