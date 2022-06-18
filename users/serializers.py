1504562c5c05b7fd8a4ccd132ed181395542424b
Sam Kosman <41757019+skosman@users.noreply.github.com>
2022-06-17 20:32:51 -0700

    12 admin user management (#43)
    * Init AppUser model and signals, update README for database shell steps
    
    * Fix method names, update enum field
    
    * Add users migration file
    
    * Init class-based views & urls, add meta class to model
    
    * Init AppUser serializer, fix prof filtering view, update urls
    
    * Remove signals from AppUser model
    
    * Update serializers, add initial serializer tests (starting point for team)
    
    * Update serializer email field, add working serializer tests
    
    * View test file creation
    
    * Fix permissions error in docker-compose.yml on linux
    
    * Add try-except block to creating objects in serializer, fix prof_type in tests
    
    * Add validate deserialization test case
    
    * Fix serializer create(), update(), add tests for both methods
    
    * Create functionality for GET at /users/
    
    * Implement user view POST functions. CURRENTLY FAILING TESTS
    
    * Implement DELETE user functionality; update unit tests; clean up views.py and test_views.py
    
    * AppUser Serializers (/users app) (#27)
    
    * Update serializer email field, add working serializer tests
    
    * Add try-except block to creating objects in serializer, fix prof_type in tests
    
    * Add validate deserialization test case
    
    * Fix serializer create(), update(), add tests for both methods
    
    * user views checks for authenticated user making request
    
    * Update AppUser serializer, test suite (#40)
    
    * Implemented Simple JWT and superuser permissions (#42)
    
    * Add simple-jwt to users app for login functionality
    
    * Update user views to contain permissions and add testing
    
    * Have default configuration for drf
    
    * Fix typo in permissions file
    
    * Fix type in permissions test
    
    * Align urls and parameters to be the same for professor id
    
    * Remove unused import
    
    * Add non-superuser test case for logging in
    
    * Fix serializer.update() hashing bug, update test suite
    
    Co-authored-by: Raphael Bassot <rbassot@shaw.ca>
    
    Co-authored-by: Raphael Bassot <rbassot@shaw.ca>
    Co-authored-by: Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
    Co-authored-by: Raphael Bassot <59449281+rbassot@users.noreply.github.com>

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,71 @@
 from rest_framework import serializers
- 
-# Create your serializers here.
+from rest_framework import viewsets
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.models import User
+from .models import AppUser
+
+#User superclass serializer
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField()
+    is_superuser = serializers.BooleanField(default=False)
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email', 'is_superuser')
+
+    #overrides default update
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            #hash the password field
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+#main AppUser serializer - User object is nested
+class AppUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    prof_type = serializers.ChoiceField(choices=AppUser.TeachingType, default=AppUser.TeachingType.TEACHING_PROF)
+    is_peng = serializers.BooleanField(default=False)
+
+    class Meta: 
+        model = AppUser 
+        fields = ('user', 'prof_type', 'is_peng')
+
+    #overrides default create
+    def create(self, validated_data):
+        """
+        Create and return a new AppUser instance, given the validated data.
+        """
+        user_data = validated_data.pop('user')
+
+        #.create_user() automatically hashes the password field
+        try:
+            user = User.objects.create_user(**user_data)
+            appUser = AppUser.objects.create(user=user, **validated_data)
+        
+        #raising a JSON-like exception
+        except ValidationError:
+            raise serializers.ValidationError({"error": "Invalid input!"})
+        return appUser
+
+    #overrides default update
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing AppUser instance, given the validated data.
+        """
+        #update the User
+        user_data = validated_data.pop('user', {})
+        user_serializer = UserSerializer(instance.user, data=user_data, partial=True)
+        user_serializer.is_valid(raise_exception=True)
+        user_serializer.update(instance.user, user_data)
+        #then update AppUser
+        super(AppUserSerializer, self).update(instance, validated_data)
+        return instance
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,234 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory    #**tests that interact with a database require subclassing of this class**
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from .models import AppUser
+from .serializers import AppUserSerializer
+from .permissions import IsAdmin
+from .views import Professor
+
+#Serializer Testing
+class AppUserSerializerTest(TestCase):
+    @classmethod
+    def setUp(self):
+        #build AppUser and AppUserSerializer instances
+        self.user_attributes = {
+            'username': 'johnd1',
+            'password': 'securepass2',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johnd123@uvic.ca',
+            'is_superuser': False
+        }
+        self.user = User.objects.create_user(**self.user_attributes)
+
+        self.app_user_attributes = {
+            'user': self.user,
+            'prof_type': 'RP',
+            'is_peng': True
+        }
+        #default data for the serializer, if needed
+        self.default_serializer_data = {
+            'user':{
+                'username': 'abcdef',
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+
+        #serialize into an AppUser object
+        self.app_user = AppUser.objects.create(**self.app_user_attributes)
+        self.serializer = AppUserSerializer(instance=self.app_user)
+
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set([
+            'user',
+            'prof_type',
+            'is_peng']))
+
+
+    def test_user_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data['user'].keys()), set([
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+            'is_superuser']))
+
+
+    def test_username_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['user']['username'], self.app_user_attributes['user'].username)
+
+
+    def test_appuser_fields_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['prof_type'], self.app_user_attributes['prof_type'])
+        self.assertEqual(data['is_peng'], self.app_user_attributes['is_peng'])
+
+    
+    def test_valid_deserialization(self):
+        serialized_data = {
+            'user':{
+                'username': 'abcdef',
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+    
+    def test_create_appuser_obj(self):
+        serialized_data = {
+            'user':{
+                'username': 'abcdef',
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+        #use the serializer to create an AppUser record, then assert it has been committed to DB
+        app_user_obj = serializer.save()
+        self.assertIsNotNone(app_user_obj.pk)
+
+    
+    def test_update_appuser_obj(self):
+        serialized_data = {
+            'user':{
+                'username': 'abcdef',
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+        #use the serializer to create an AppUser record
+        app_user_obj = serializer.save()
+        obj_key = app_user_obj.pk
+        
+        #now, update the AppUser record order by referencing an existing instance
+        updated_serialized_data = {
+            'user':{
+                'username': 'ghijkl',   #updated field
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'RP',   #updated field
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(instance=app_user_obj, data=updated_serialized_data)
+        self.assertTrue(serializer.is_valid())
+        updated_app_user = serializer.save()
+        updated_obj_key = updated_app_user.pk
+
+        #assert that the same instance was updated, and updated as expected
+        self.assertEquals(updated_obj_key, obj_key)
+        self.assertEquals(AppUser.objects.get(pk=obj_key).user.username, updated_serialized_data['user']['username'])
+        self.assertEquals(AppUser.objects.get(pk=obj_key).prof_type, updated_serialized_data['prof_type'])
+
+
+    def test_updated_appuser_password_hash(self):
+        serialized_data = {
+            'user':{
+                'username': 'abcdef',
+                'password': '123',
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+        #use the serializer to create an AppUser record
+        app_user_obj = serializer.save()
+        obj_key = app_user_obj.pk
+        
+        #now, update the AppUser record order by referencing an existing instance
+        updated_serialized_data = {
+            'user':{
+                'username': 'abcdef',   
+                'password': 'newpass!',  #only updated field
+                'first_name': 'Abc',
+                'last_name': 'Def',
+                'email': 'abc@uvic.ca',
+                'is_superuser': False
+            },
+            'prof_type': 'TP',
+            'is_peng': True
+        }
+        serializer = AppUserSerializer(instance=app_user_obj, data=updated_serialized_data)
+        self.assertTrue(serializer.is_valid())
+        updated_app_user = serializer.save()
+        updated_obj_key = updated_app_user.pk
+
+        #assert that the same instance was updated, and updated as expected
+        self.assertEquals(updated_obj_key, obj_key)
+        #check that the password is now associated and correctly hashed
+        self.assertTrue(AppUser.objects.get(pk=obj_key).user.check_password('newpass!'))
+        
+        
+
+class UserPermissions(TestCase):
+    """ Test Permissions
+        IsAdmin should return true only if user is superuser
+    """
+    @classmethod
+    def setUp(self):
+        self.admin_user = User.objects.create(username='admin', is_superuser=True, id='1')
+        self.non_admin_user = User.objects.create(username='prof', is_superuser=False, id='12')
+        self.factory = RequestFactory()
+        self.view = Professor.as_view()
+
+    def test_admin_user_permissions(self):
+        request = self.factory.delete('/users/abcdef')
+        request.user = self.admin_user
+        permission_check = IsAdmin()
+        permission = permission_check.has_permission(request, None)
+        self.assertTrue(permission)
+        obj_permissions = permission_check.has_object_permission(request, self.view, self.admin_user)
+        self.assertTrue(obj_permissions)
+    
+    def test_non_admin_user_permissions(self):
+        request = self.factory.delete('/users/abcdef')
+        request.user = self.non_admin_user
+        permission_check = IsAdmin()
+        permission = permission_check.has_permission(request, None)
+        self.assertFalse(permission)
+        obj_permissions = permission_check.has_object_permission(request, self.view, self.non_admin_user)
+        self.assertFalse(obj_permissions)
+        
+        
