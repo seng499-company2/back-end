4dae1da4937db86d09908531acf5cf178a2ed1ca
Raphael Bassot <rbassot@shaw.ca>
2022-06-18 15:12:51 -0700

    Fix Preferences fields constraints, add admin.py, add migrations files

--- preferences/models.py
+++ preferences/models.py
@@ -4,33 +4,37 @@ from users.models import AppUser
 
 from django.utils.translation import gettext_lazy as _
 
-# Model for the database representation of a teacher Preference record.
-class Preference(models.Model):
+# Model for the database representation of a teacher Preferences record.
+class Preferences(models.Model):
 
     class SabbaticalLength(models.TextChoices):
         HALF_LENGTH = 'HALF', _('Half Length')
         FULL_LENGTH = 'FULL', _('Full Length')
         NONE = 'NONE', _('None')
 
-    user_id = models.OneToOneField(
+    appuser = models.OneToOneField(
         AppUser,
         on_delete=models.CASCADE,
         primary_key=True
     )
-    is_completed = models.BooleanField(default=False)
-    unavailable_sem1 = models.CharField(max_length=7)   #'YYYY-MM'
-    unavailable_sem2 = models.CharField(max_length=7)   #'YYYY-MM'
-    num_relief_courses = models.PositiveIntegerField()
+    is_submitted = models.BooleanField(default=False)
+    unavailable_sem1 = models.CharField(max_length=7, blank=True, default='')   #'YYYY-MM'
+    unavailable_sem2 = models.CharField(max_length=7, blank=True, default='')   #'YYYY-MM'
+    num_relief_courses = models.PositiveIntegerField(default=0)
     taking_sabbatical = models.BooleanField(default=False)
     sabbatical_length = models.CharField(
         max_length=4,
         choices=SabbaticalLength.choices,
         default=SabbaticalLength.NONE
     )
-    sabbatical_start_month = models.PositiveIntegerField()
-    preferred_hours = models.TextField()        #holds stringified JSON object
-    teaching_willingness = models.TextField()   #holds stringified JSON object
-    teaching_difficulty = models.TextField()
+    sabbatical_start_month = models.PositiveIntegerField(blank=True)
+    preferred_hours = models.TextField(blank=True)        #holds stringified JSON object
+    teaching_willingness = models.TextField(blank=True)   #holds stringified JSON object
+    teaching_difficulty = models.TextField(blank=True)
     wants_topics_course = models.BooleanField(default=False)
-    topics_course_id = models.CharField(max_length=20)
-    topics_course_name = models.CharField(max_length=255)
+    topics_course_id = models.CharField(max_length=20, blank=True, default='')
+    topics_course_name = models.CharField(max_length=255, blank=True, default='')
+
+    class Meta:
+        managed = True  #auto creates tables
+        db_table = 'preferences'
