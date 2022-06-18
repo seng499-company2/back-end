60637d09c689a1165a442e2e93a4c2f115e48efd
benguarasci <benguarasci@icloud.com>
2022-06-18 12:30:56 -0700

    frontend model draft

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DisplayCourse(models.Model):
+    course_code = models.CharField(max_length=7)
+    course_title = models.TextField(max_length=30)
+    fall_offering = models.BooleanField()
+    spring_offering = models.BooleanField()
+    summer_offering = models.BooleanField()
+    PENG_required = models.BooleanField()
