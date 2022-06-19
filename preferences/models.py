e6d1d4988fc7daea98a101acd96cc09a9b000413
Raphael Bassot <rbassot@shaw.ca>
2022-06-18 18:14:34 -0700

    Rename professor field for clarity, fix integer default field

--- preferences/models.py
+++ preferences/models.py
@@ -12,9 +12,10 @@ class Preferences(models.Model):
         FULL_LENGTH = 'FULL', _('Full Length')
         NONE = 'NONE', _('None')
 
-    appuser = models.OneToOneField(
+    professor = models.OneToOneField(
         AppUser,
         on_delete=models.CASCADE,
+        to_field='user',
         primary_key=True
     )
     is_submitted = models.BooleanField(default=False)
@@ -27,7 +28,7 @@ class Preferences(models.Model):
         choices=SabbaticalLength.choices,
         default=SabbaticalLength.NONE
     )
-    sabbatical_start_month = models.PositiveIntegerField(blank=True)
+    sabbatical_start_month = models.PositiveIntegerField(default=0)
     preferred_hours = models.TextField(blank=True)        #holds stringified JSON object
     teaching_willingness = models.TextField(blank=True)   #holds stringified JSON object
     teaching_difficulty = models.TextField(blank=True)
