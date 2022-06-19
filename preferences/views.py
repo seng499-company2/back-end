337db7d311fe15d20456f64215605a810ba927bd
Raphael Bassot <rbassot@shaw.ca>
2022-06-18 18:48:48 -0700

    Initialize views.py (incomplete), permissions.py

--- preferences/views.py
+++ preferences/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.http import HttpRequest
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+import preferences
+from .models import Preferences
+from .serializers import PreferencesSerializer
+from .permissions import IsAdmin
+
+
+class PreferencesRecord(APIView):
+    """
+    Administrator Preferences: Get a professor's preferences record, or update an existing professor record.
+    """
+    
+    permission_classes = [IsAdmin, IsAuthenticated]
+
+    # (Admin) return the unique Preferences record for a professor.
+    def get(self, request: HttpRequest, professor_id: str):
+        if request.method != "GET":
+            return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        try:
+            preferences_record = Preferences.objects.filter(professor__user__username=professor_id)
+            if preferences_record is None or not isinstance(preferences_record, Preferences):
+                return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        except preferences.models.Preferences.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        request_data = JSONParser().parse(request)
+        serializer = PreferencesSerializer(preferences_record, data=request_data) #TODO: mock or build the serializer
+        if serializer.is_valid():
+            serializer.update(preferences_record, serializer.validated_data)
+            return HttpResponse(json.dumps(serializer.data), status=status.HTTP_200_OK)
+        return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # (Admin) update the unique Preferences record for a professor.
+    def post(self, request: HttpRequest, format=None) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
