eae027994458984f4940b6be71f9ef8b23c02601
Raphael Bassot <rbassot@shaw.ca>
2022-06-22 13:31:29 -0700

    Fix serialization by overriding update(), create() methods, add test suite

--- preferences/tests.py
+++ preferences/tests.py
@@ -76,6 +76,132 @@ class PreferencesSerializerTest(TestCase):
             'topics_course_name']))
 
     
-    def test_professor_field_converts_to_string(self):
+    def test_professor_field_serializes_to_string(self):
         data = self.serializer.data
         self.assertIsInstance(data['professor'], str)
+
+
+    '''def test_get_appuser_obj(self):
+        data = self.serializer.data
+        prof_obj = AppUser.objects.get(user__username="johnd1")
+        print(prof_obj.user.username)'''
+
+
+    def test_valid_deserialization(self):
+        serialized_data = {
+            "professor": "johnd1",
+            "is_submitted": True,
+            "is_unavailable_sem1": False,
+            "is_unavailable_sem2": True,
+            "num_relief_courses": 1,
+            "taking_sabbatical": True,
+            "sabbatical_length": "FULL",
+            "sabbatical_start_month": 1,
+            "preferred_hours": [
+                {"Mon": "8am-9am"},
+                {"Thu": "1pm-2pm"}
+            ],
+            "teaching_willingness": {
+                "CSC226": "Very Willing"
+            },
+            "teaching_difficulty": {
+                "CSC226": "Able"
+            },
+            "wants_topics_course": True,
+            "topics_course_id": "CSC485c",
+            "topics_course_name": "Data Management and Parallelization"
+        }
+        serializer = PreferencesSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+
+    def test_create_preferences_obj(self):
+        #build a new AppUser instance
+        user_attributes = {
+            'username': 'julia2',
+            'password': 'securepass2',
+            'first_name': 'Julia',
+            'last_name': 'May',
+            'email': 'juliam456@uvic.ca',
+            'is_superuser': False
+        }
+        user = User.objects.create_user(**user_attributes)
+
+        app_user_attributes = {
+            'user': user,
+            'prof_type': 'RP',
+            'is_peng': True
+        }
+        app_user = AppUser.objects.create(**app_user_attributes)
+        app_user_serializer = AppUserSerializer(instance=app_user)
+
+        #create a new Preferences object using the serializer
+        serialized_data = {
+            "professor": "julia2",
+            "is_submitted": True,
+            "is_unavailable_sem1": False,
+            "is_unavailable_sem2": True,
+            "num_relief_courses": 1,
+            "taking_sabbatical": True,
+            "sabbatical_length": "FULL",
+            "sabbatical_start_month": 1,
+            "preferred_hours": [
+                {"Mon": "8am-9am"},
+                {"Thu": "1pm-2pm"}
+            ],
+            "teaching_willingness": {
+                "CSC226": "Very Willing"
+            },
+            "teaching_difficulty": {
+                "CSC226": "Able"
+            },
+            "wants_topics_course": True,
+            "topics_course_id": "CSC485c",
+            "topics_course_name": "Data Management and Parallelization"
+        }
+        serializer = PreferencesSerializer(data=serialized_data)
+        self.assertTrue(serializer.is_valid())
+
+        #use the serializer to create an AppUser record, then assert it has been committed to DB
+        preferences_obj = serializer.save()
+        self.assertIsNotNone(preferences_obj.pk)
+
+
+    def test_update_preferences_obj(self):
+        #get an existing Preferences object
+        preferences_obj = Preferences.objects.get(professor__user__username="johnd1")
+        obj_key = preferences_obj.pk
+
+        #update the Preferences record order by referencing an existing instance
+        updated_serialized_data = {
+            "professor": "johnd1",
+            "is_submitted": False,      #updated
+            "is_unavailable_sem1": False,
+            "is_unavailable_sem2": True,
+            "num_relief_courses": 1,
+            "taking_sabbatical": True,
+            "sabbatical_length": "HALF", #updated
+            "sabbatical_start_month": 1,
+            "preferred_hours": [
+                {"Mon": "10am-11am"}    #updated JSON
+            ],
+            "teaching_willingness": {
+                "CSC226": "Very Willing"
+            },
+            "teaching_difficulty": {
+                "CSC226": "Able"
+            },
+            "wants_topics_course": True,
+            "topics_course_id": "CSC485c",
+            "topics_course_name": "Data Management and Parallelization"
+        }
+        serializer = PreferencesSerializer(instance=preferences_obj, data=updated_serialized_data)
+        self.assertTrue(serializer.is_valid())
+        updated_preferences_obj = serializer.save()
+        updated_obj_key = updated_preferences_obj.pk
+
+        #assert that the same instance was updated, and updated as expected
+        self.assertEquals(updated_obj_key, obj_key)
+        self.assertEquals(Preferences.objects.get(pk=obj_key).is_submitted, updated_serialized_data['is_submitted'])
+        self.assertEquals(Preferences.objects.get(pk=obj_key).sabbatical_length, updated_serialized_data['sabbatical_length'])
+        self.assertEquals(Preferences.objects.get(pk=obj_key).preferred_hours, updated_serialized_data['preferred_hours'])
