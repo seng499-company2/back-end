3ac37bfcc4bd4bbc49ed0b9c670ad1b0fd749d41
benguarasci <benguarasci@icloud.com>
2022-06-24 12:12:45 -0700

    added urls and list all courses view

--- courses/urls.py
+++ courses/urls.py
@@ -1,3 +1,8 @@
 from django.urls import include, path
 
-# Define your urls here.
+urlpatterns = [
+    #/courses
+    path('courses/', views.ListCourses.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import CourseSerializer
+from .serializers import 
+from django.http import HttpResponse
+from django.http import HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
+
+
+class ListCourses(APIView):
+
+    def get(self, request):
+            if request.method != "GET":
+                return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            # get all non-admin AppUsers **may have to also fetch User parent class + concatenate fields**
+            serializer = AppUserSerializer(Courses.objects, many=True)
+            return HttpResponse(json.dumps(serializer.data), status=status.HTTP_200_OK)
+
 
 # Create your views here.
+
+
