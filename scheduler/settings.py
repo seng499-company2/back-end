9f416a867289d2b44736110e878e69f7dd960653
Sam Kosman <41757019+skosman@users.noreply.github.com>
2022-06-24 14:46:19 -0700

    Add cors to scheduler app (#58)

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -49,11 +49,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# TODO: Will need to add the url of the FE app once it is up
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'scheduler.urls'
