ccd9ba0e588ad9bf14e74377462b71568bbc2ba1
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-07-02 11:06:44 -0700

    Implement and test course_to_dict in adapter.py

--- schedule/adapter.py
+++ schedule/adapter.py
@@ -1,4 +1,13 @@
-from courses.models import cour
+import typing
 
-def course_to_dictionary(course: Couse) -> []:
-    return 0
+from courses.models import Course
+
+
+def course_to_dictionary(course: Course) -> None or typing.Dict[str, str or bool]:
+    if course is None:
+        return None
+    course_dict = vars(course)
+    # the state of the django.models object should not be returned
+    del(course_dict["_state"])
+    return course_dict
+
--- schedule/tests.py
+++ schedule/tests.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import SlidingToken
 from django.contrib.auth.models import User
+from schedule.adapter import course_to_dictionary
+from courses.models import Course
 
 
 class ViewTest(TestCase):
@@ -49,3 +51,36 @@ class ViewTest(TestCase):
         self.assertIsNotNone(response)
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         self.assertContains(response, "GENERATED SCHEDULE FROM COMPANY 2 ALGORITHM")
+
+# ADAPTER TESTS
+    def test_none(self):
+        course_dict = course_to_dictionary(None)
+        self.assertIsNone(course_dict)
+
+    def test_trivial(self):
+        course_attributes = {
+            "course_code": "SENG499",
+            "section": "A01",
+            "course_title": "Design Project 2",
+            "fall_offering": True,
+            "spring_offering": True,
+            "summer_offering": False,
+            "PENG_required": True
+        }
+        course = Course.objects.create(**course_attributes)
+        course_dict = course_to_dictionary(course)
+        self.assertIsNotNone(course_dict)
+        self.assertEquals("SENG499", course_dict["course_code"])
+        self.assertEquals("A01", course_dict["section"])
+        self.assertEquals("Design Project 2", course_dict["course_title"])
+        self.assertEquals(True, course_dict["fall_offering"])
+        self.assertEquals(True, course_dict["spring_offering"])
+        self.assertEquals(False, course_dict["summer_offering"])
+        self.assertEquals(True, course_dict["PENG_required"])
+        try:
+            state = course_dict["_state"]
+            # Should have thrown keyError
+            self.fail()
+        except KeyError:
+            # expected behaviour is throwing a KeyError
+            pass
