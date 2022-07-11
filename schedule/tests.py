6a597a84256e37484d2afddee1a81e72ae0ba1c5
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-07-10 20:41:00 -0700

    Setup creat-schedule test case

--- schedule/tests.py
+++ schedule/tests.py
@@ -8,6 +8,8 @@ from courses.models import Course
 from schedule.alg_data_generator import get_historic_course_data
 from schedule.alg_data_generator import get_program_enrollment_data
 from schedule.alg_data_generator import get_schedule
+from schedule.Schedule_models import A_Schedule, A_TimeSlot, A_CourseSection, A_Course, A_CourseOffering
+from schedule.Schedule_serializers import A_ScheduleSerializer
 
 
 class ViewTest(TestCase):
@@ -112,8 +114,52 @@ class ViewTest(TestCase):
 
         schedule = get_schedule()
         expected = {'fall': [{'course': {'code': 'SENG499', 'title': 'Design Project 2', 'pengRequired': {'fall': False, 'spring': True, 'summer': True}, 'yearRequired': 4}, 'sections': [{'professor': '', 'capacity': '', 'timeslots': ''}, {'professor': '', 'capacity': '', 'timeslots': ''}]}], 'spring': [{'course': {'code': 'SENG499', 'title': 'Design Project 2', 'pengRequired': {'fall': False, 'spring': True, 'summer': True}, 'yearRequired': 4}, 'sections': [{'professor': '', 'capacity': '', 'timeslots': ''}, {'professor': '', 'capacity': '', 'timeslots': ''}]}], 'summer': [{'course': {'code': 'SENG499', 'title': 'Design Project 2', 'pengRequired': {'fall': False, 'spring': True, 'summer': True}, 'yearRequired': 4}, 'sections': [{'professor': '', 'capacity': '', 'timeslots': ''}, {'professor': '', 'capacity': '', 'timeslots': ''}]}]}
-
         self.assertDictEqual(expected, schedule)
 
-    def test_get_professor_dict(self):
-        pass
+    def test_get_schedule_many_courses(self):
+        saved_schedule = self.get_schedule_object()
+        schedule = get_schedule()
+        expected = {}
+        self.assertEquals(expected, schedule)
+
+
+    def get_schedule_object(self):
+        #build TimeSlot objects
+        t1 = A_TimeSlot(dayOfWeek='MONDAY', timeRange='("12:00","13:00")')
+        t2 = A_TimeSlot(dayOfWeek='TUESDAY', timeRange='("14:00","15:00")')
+        t1.save()   #NOTE: Many-to-many relationships cannot be created without saving BOTH entity objects first
+        t2.save()
+
+        #build CourseSection objects
+        cs1 = A_CourseSection(professor={"id": "mzastre", "name":"Michael Zastre"}, capacity=0)
+        cs1.save()
+        cs1.timeSlots.add(t1, t2)
+
+        #build Course objects
+        c1 = A_Course(code="CSC225", title="Algorithms and Data Structures I", pengRequired={"fall": True, "spring": False, "summer": True}, yearRequired=3)
+        c1.save()
+
+        #build CourseOffering objects
+        co_fall = A_CourseOffering()
+        co_fall.save()
+        co_fall.course.add(c1)
+        co_fall.sections.add(cs1)
+
+        co_spring = A_CourseOffering()
+        co_spring.save()
+        co_spring.course.add(c1)
+        co_spring.sections.add(cs1)
+
+        co_summer = A_CourseOffering()
+        co_summer.save()
+        co_summer.course.add(c1)
+        co_summer.sections.add(cs1)
+
+        #finally, build Schedule object
+        schedule = A_Schedule()
+        schedule.save()
+        schedule.fall.add(co_fall)
+        schedule.spring.add(co_spring)
+        schedule.summer.add(co_summer)
+        schedule.save()
+        return schedule
