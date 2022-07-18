a95332efb0fd300ef5fa78663c6d39abd46659ba
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-07-18 12:42:51 -0700

    Created integration-test.sh

---
+++ integration-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo '------------------------------------'
+echo '|    Starting Integration Test     |'
+echo '------------------------------------'
+
+docker-compose up -d
+
+sleep 3
+
+
+
