167bcca521f68b60563a90d109b04d4852a31bdb
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-07-18 13:58:35 -0700

    Add integration-test.sh to repo

--- integration-test.sh
+++ integration-test.sh
@@ -8,15 +8,40 @@ docker-compose up -d
 
 sleep 3
 
-./init_db.sh
+#./init_db.sh
 
 sleep 3
 
-docker-compose exec web python manage.py test integration_tests
+echo ' '
+echo '------------------------------------'
+echo '|        Testing Company 2          |'
+echo '------------------------------------'
+echo ' '
+echo ' '
 
-docker-compose down
+curl http://localhost:8000/schedule/2022/FALL/2
+
+echo ' '
+echo ' '
+echo ' '
+echo '------------------------------------'
+echo '|        Testing Company 1          |'
+echo '------------------------------------'
+echo ' '
+echo ' '
+echo ' '
+
+curl http://localhost:8000/schedule/2022/FALL/1
 
 
+echo ' '
+echo ' '
+echo ' '
 echo '------------------------------------'
 echo '|        Testing Complete          |'
 echo '------------------------------------'
+echo ' '
+echo ' '
+echo ' '
+
+docker-compose down
