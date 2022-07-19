4eceab6972bfc1985421107febc2475614fc90a0
Samantha Kosman <skosman@uvic.ca>
2022-07-19 09:15:09 -0700

    fix tests to work with schedule by initializing prof

--- schedule/tests.py
+++ schedule/tests.py
@@ -62,6 +62,96 @@ class ViewTest(TestCase):
             self.course2 = Course.objects.create(**course_attributes)
             alg_course_offering = course_to_course_offering(self.course2)
             add_course_offering_to_schedule(self.course, alg_course_offering)
+    def init_prof(self):
+        #build AppUser instance
+        self.user_attributes = {
+            'username': 'johnd1',
+            'password': 'securepass2',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johnd123@uvic.ca',
+            'is_superuser': False
+        }
+        self.user = User.objects.create_user(**self.user_attributes)
+
+        self.app_user_attributes = {
+            'user': self.user,
+            'prof_type': 'TP',
+            'is_peng': True,
+            'is_form_submitted': True
+        }
+        self.app_user = AppUser.objects.create(**self.app_user_attributes)
+        self.app_user_serializer = AppUserSerializer(instance=self.app_user)
+
+        #create associated Preferences record
+        self.preferences_attributes = {
+            "professor": self.app_user,
+            "is_submitted": True,
+            "taking_sabbatical": False,
+            "sabbatical_length": "NONE",
+            "sabbatical_start_month": 0,
+            "preferred_times": {
+                "fall": {
+                        "monday": [
+                              ["8:30", "18:30" ],
+                              ["19:30", "20:30"]
+                        ],
+                        "tuesday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "wednesday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "thursday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "friday": [
+                              ["8:30", "18:30"]
+                        ]
+                  },
+                  "spring": {
+                        "monday": [
+                            ["8:30", "18:30"]
+                        ],
+                        "tuesday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "wednesday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "thursday": [
+                              ["8:30", "18:30"]
+                        ],
+                        "friday": [
+                              ["8:30","18:30"]
+                        ]
+                  },
+                  "summer": {
+                  }
+            },
+            "courses_preferences": {
+                    "SENG321": {
+                        "willingness": 2,
+                        "difficulty": 1
+                    },
+                    "SENG499": {
+                        "willingness": 3,
+                        "difficulty": 2
+                    }
+            },
+            "preferred_non_teaching_semester": "summer",
+            "preferred_courses_per_semester": {
+                  "fall": 2,
+                  "spring": 1,
+                  "summer": 0
+            },
+
+            "preferred_course_day_spreads": [
+                   "TWF", "T", "W", "F" 
+            ],
+        }
+        
+        Preferences.objects.update(**self.preferences_attributes)
 
     def get_course1_ordered_dict(self):
         expected = OrderedDict()
@@ -140,6 +230,7 @@ class ViewTest(TestCase):
             return
         self.init_course1()
         self.init_course2()
+        self.init_prof()
         response = self.client.get('/schedule/2022/FALL/2', format='json')
         self.assertIsNotNone(response)
         self.assertEquals(status.HTTP_200_OK, response.status_code)
@@ -149,6 +240,7 @@ class ViewTest(TestCase):
             return
         self.init_course1()
         self.init_course2()
+        self.init_prof()
         response = self.client.get('/schedule/2022/FALL/1', format='json')
         self.assertIsNotNone(response)
         self.assertEquals(status.HTTP_200_OK, response.status_code)
@@ -204,149 +296,59 @@ class ViewTest(TestCase):
         schedule = get_schedule()
         self.assertEquals(expected, schedule)
 
-class GetProfessorDictTest(TestCase):
-    @classmethod
-    def setUp(self): 
-        self.maxDiff = None
-        #build AppUser instance
-        self.user_attributes = {
-            'username': 'johnd1',
-            'password': 'securepass2',
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'email': 'johnd123@uvic.ca',
-            'is_superuser': False
-        }
-        self.user = User.objects.create_user(**self.user_attributes)
-
-        self.app_user_attributes = {
-            'user': self.user,
-            'prof_type': 'TP',
-            'is_peng': False,
-            'is_form_submitted': True
-        }
-        self.app_user = AppUser.objects.create(**self.app_user_attributes)
-        self.app_user_serializer = AppUserSerializer(instance=self.app_user)
-
-        #create associated Preferences record
-        self.preferences_attributes = {
-            "professor": self.app_user,
-            "is_submitted": True,
-            "taking_sabbatical": True,
-            "sabbatical_length": "HALF",
-            "sabbatical_start_month": 5,
-            "preferred_times": {
-                "fall": {
-                        "monday": [
-                              ["8:30", "18:30" ],
-                              ["19:30", "20:30"]
-                        ],
-                        "tuesday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "wednesday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "thursday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "friday": [
-                              ["8:30", "18:30"]
-                        ]
-                  },
-                  "spring": {
-                        "monday": [],
-                        "tuesday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "wednesday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "thursday": [
-                              ["8:30", "18:30"]
-                        ],
-                        "friday": [
-                              ["8:30","18:30"]
-                        ]
-                  },
-                  "summer": {}
-            },
-            "courses_preferences": {
-                    "CSC111": {
-                        "willingness": 2,
-                        "difficulty": 1
-                    },
-                    "CSC115": {
-                        "willingness": 1,
-                        "difficulty": 2
-                    }
-            },
-            "preferred_non_teaching_semester": "summer",
-            "preferred_courses_per_semester": {
-                  "fall": 2,
-                  "spring": 1,
-                  "summer": 0
-            },
-
-            "preferred_course_day_spreads": [
-                   "TWF", "T", "W", "F" 
-            ],
-        }
-        
-        Preferences.objects.update(**self.preferences_attributes)
-
-        
     def test_get_professor_dict(self):
+        self.init_prof()
         result = get_professor_dict()
-        
-        expected = {
+        expected = [{
             "id": str(self.user.id),
             "name": "John Doe",
-            "isPeng": False,
+            "isPeng": True,
             "facultyType": "TEACHING",
-            "teachingObligations": 3,
+            "teachingObligations": 6,
             "coursePreferences": [
                   {
-                        "courseCode": "CSC111",
+                        "courseCode": "SENG321",
                         "enthusiasmScore": 78
                   },
                   {
-                        "courseCode": "CSC115",
-                        "enthusiasmScore": 20
+                        "courseCode": "SENG499",
+                        "enthusiasmScore": 100
                   },
             ],
             "preferredTimes": {
-                  "fall": {
+                 "fall": {
                         "monday": [
-                              ["8:30", "18:30" ],
-                              ["19:30", "20:30"]
+                            ["8:30", "18:30" ],
+                            ["19:30", "20:30"]
                         ],
                         "tuesday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "wednesday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "thursday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "friday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ]
                   },
                   "spring": {
-                        "monday": [],
+                        "monday": [
+                            ["8:30", "18:30"]
+                        ],
                         "tuesday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "wednesday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "thursday": [
-                              ["8:30", "18:30"]
+                            ["8:30", "18:30"]
                         ],
                         "friday": [
-                              ["8:30","18:30"]
+                            ["8:30","18:30"]
                         ]
                   },
                   "summer": {}
@@ -360,5 +362,6 @@ class GetProfessorDictTest(TestCase):
             "preferredCourseDaySpreads": [
                   "TWF", "T", "W", "F" 
             ]
-        }
-        self.assertEqual(expected, result[0])
+        }]
+        
+        self.assertEqual(expected, result)
